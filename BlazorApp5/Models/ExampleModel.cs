7d55115042837bdaa0dc9d30f72a781ec408ec74
yurim.choi <yurim.choi@bizentro.com>
2024-11-05 16:30:41 +0900

    중첩 컴포넌트

---
+++ BlazorApp5/Models/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp5.Models
+{
+    public class ExampleModel
+    {
+        [Required (ErrorMessage = "이름은 필수 입니다.")]
+        [StringLength(10,ErrorMessage = "이름을 10자이하로 입력하세요.")]
+        public string Name { get; set; }
+    }
+}
